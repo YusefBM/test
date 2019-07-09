a624b60b9bb2c951898052fbaf3cb10e439ff990
dperezcabrera <dperezcabrera@gmail.com>
2019-07-10 00:38:26 +0200

    Utilizar credenciales almacenados en la bbdd

--- src/main/java/com/github/dperezcabrera/test/Initializer.java
+++ src/main/java/com/github/dperezcabrera/test/Initializer.java
@@ -4,7 +4,6 @@ import com.github.dperezcabrera.test.architecture.auth.dtos.UserDto;
 import com.github.dperezcabrera.test.architecture.auth.entities.Role;
 import com.github.dperezcabrera.test.architecture.auth.repositories.UserRepository;
 import com.github.dperezcabrera.test.architecture.auth.repositories.RoleRepository;
-import com.github.dperezcabrera.test.architecture.auth.repositories.PermissionRepository;
 import com.github.dperezcabrera.test.architecture.auth.services.UserService;
 import java.util.Arrays;
 import org.springframework.boot.CommandLineRunner;
@@ -18,26 +17,24 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class Initializer implements CommandLineRunner {
 
-    private UserService userService;
+	private UserService userService;
 
-    private RoleRepository roleRepository;
+	private RoleRepository roleRepository;
 
-    private UserRepository userRepository;
+	private UserRepository userRepository;
 
-    private PermissionRepository permissionRepository;
-
-    @Override
-    @Transactional
-    public void run(String... strings) throws Exception {
+	@Override
+	@Transactional
+	public void run(String... strings) throws Exception {
 		if (roleRepository.count() == 0) {
-			roleRepository.save(new Role("admin" , "Administrator"));
-			roleRepository.save(new Role("user" , "User"));
+			roleRepository.save(new Role("admin", "Administrator"));
+			roleRepository.save(new Role("user", "User"));
 		}
-        if (userRepository.count() == 0){
-			userService.addUser(new UserDto("marina", "Marina", "marina@mail.com", "password", Arrays.asList("user")));
-			userService.addUser(new UserDto("david", "David", "david@mail.com", "password", Arrays.asList("admin")));
+		if (userRepository.count() == 0) {
+			userService.addUser(new UserDto("marina", "Marina", "marina@mail.com", "1", Arrays.asList("user")));
+			userService.addUser(new UserDto("david", "David", "david@mail.com", "1", Arrays.asList("admin")));
 		}
-              
-        log.info("\"david\" -> {}",userService.getUser("david"));
-    }
+
+		log.info("\"david\" -> {}", userService.getUser("david"));
+	}
 }
--- src/main/java/com/github/dperezcabrera/test/architecture/auth/services/CredentialService.java
+++ src/main/java/com/github/dperezcabrera/test/architecture/auth/services/CredentialService.java
@@ -1,21 +1,33 @@
 package com.github.dperezcabrera.test.architecture.auth.services;
 
+import com.github.dperezcabrera.test.architecture.auth.repositories.UserRepository;
 import com.google.common.hash.Hashing;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class CredentialService {
 
+	private final UserRepository userRepository;
+	
 	public String generateSalt() {
 		return UUID.randomUUID().toString();
 	}
 
 	public String hash(@NonNull String username, @NonNull String password, @NonNull String salt) {
-		return Hashing.sha512().hashString(String.join(":", salt, username, password), StandardCharsets.UTF_8).toString();
+		return Hashing.sha512().hashString(String.join(":", salt, username.toLowerCase(), password), StandardCharsets.UTF_8).toString();
+	}
+	
+	public boolean check(@NonNull String login, @NonNull String password){
+		String username = login.toLowerCase();
+		return userRepository.findById(username)
+				.map(u -> u.getHash().equals(hash(username, password, u.getSalt())))
+				.orElse(Boolean.FALSE);
 	}
 }
