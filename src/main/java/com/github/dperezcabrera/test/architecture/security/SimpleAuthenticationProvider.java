b4fd10d413247614e23cb285fd6d0be6f2bcae4b
dperezcabrera <dperezcabrera@gmail.com>
2019-07-10 00:37:47 +0200

    Simplificar interfaz web

--- src/main/java/com/github/dperezcabrera/test/architecture/security/SimpleAuthenticationProvider.java
+++ src/main/java/com/github/dperezcabrera/test/architecture/security/SimpleAuthenticationProvider.java
@@ -1,7 +1,9 @@
 package com.github.dperezcabrera.test.architecture.security;
 
+import com.github.dperezcabrera.test.architecture.auth.services.CredentialService;
 import com.github.dperezcabrera.test.architecture.logging.Logging;
 import java.util.ArrayList;
+import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -9,14 +11,17 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class SimpleAuthenticationProvider implements AuthenticationProvider {
 
+	private CredentialService credentialService;
+	
     @Logging(disabled = true)
     @Override
     public Authentication authenticate(Authentication authentication) {
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
-        if (!username.isEmpty() && "1".equals(password)) {
+        if (credentialService.check(username, password)) {
             return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
         } else {
             throw new BadCredentialsException("Wrong credentials");
