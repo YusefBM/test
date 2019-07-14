a0fd7f1ae18a2b8388c49ae974af96ec21690b42
dperezcabrera <dperezcabrera@gmail.com>
2019-07-14 12:38:45 +0200

    Eliminar ruta absoluta hasta el docker-compose en script

--- .scripts/run-docker.sh
+++ .scripts/run-docker.sh
@@ -8,7 +8,7 @@ mvn clean package
 
 docker build -t dperezcabrera/test -f docker/Dockerfile .
 
-/usr/local/bin/docker-compose -f docker/docker-compose.yml up
+docker-compose -f docker/docker-compose.yml up
 
-# /usr/local/bin/docker-compose -f docker/docker-compose.yml down
+# docker-compose -f docker/docker-compose.yml down
 # docker exec -it postgres psql --username postgres --dbname test
